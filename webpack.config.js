556825a9e5e6c8d784cc8f05644366fc25def0e8
jconnorbuilds <65531832+jconnorbuilds@users.noreply.github.com>
2024-05-14 18:34:34 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+    watchFiles: ['./src/**/*.html', './src/**/*.**css'],
+  },
+  module: {
+    rules: [{ test: /\.s[ac]ss/, use: ['style-loader', 'css-loader', 'sass-loader'] }],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new ESLintPlugin({
+      configType: 'flat',
+    }),
+  ],
+};
