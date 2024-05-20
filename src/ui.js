96f09f4021a6f053028f7346f1c761eff5c2b2d4
Joe Connor <joseph.connor.music@gmail.com>
2024-05-20 09:36:11 +0900

    Adds weather icons

--- src/ui.js
+++ src/ui.js
@@ -9,47 +9,60 @@ const currentUnit = '°C';
  * Icons from https://erikflowers.github.io/weather-icons/
  */
 const WEATHER_ICON_CLASSES = {
-  1000: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1003: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1006: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1009: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1030: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1063: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1066: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1069: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1072: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1087: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1114: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1117: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1135: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1147: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1150: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1153: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1168: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1171: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1180: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1183: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1186: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1189: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1192: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1195: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1198: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1201: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1204: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1207: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1210: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1213: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1216: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1219: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1222: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1225: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1258: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1282: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1261: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1264: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1273: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1276: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
-  1279: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' },
+  1000: { day: 'wi wi-day-sunny', night: 'wi wi-night-clear' }, // Sunny / Clear
+  1003: { day: 'wi wi-day-cloudy-high', night: 'wi wi-night-alt-partly-cloudy' }, // Partly cloudy
+  1006: { day: 'wi wi-cloudy', night: 'wi wi-night-alt-cloudy' }, // Cloudy
+  1009: { day: 'wi wi-day-sunny-overcast', night: 'wi wi-night-alt-cloudy' }, // Overcast
+  1030: { day: 'wi wi-day-fog', night: 'wi wi-night-fog' }, // Mist
+  1063: { day: 'wi wi-day-showers', night: 'wi wi-night-alt-showers' }, // Patchy rain possible
+  1066: { day: 'wi wi-day-snow', night: 'wi wi-night-alt-snow' }, // Patchy snow possible
+  1069: { day: 'wi wi-day-rain-mix', night: 'wi wi-night-alt-rain-mix' }, // Patchy sleet possible
+  1072: { day: 'wi wi-day-sleet', night: 'wi wi-night-alt-sleet' }, // Patchy freezing drizzle possible
+  1087: { day: 'wi wi-day-lightning', night: 'wi wi-night-alt-lightning' }, // Thundery outbreaks possible
+  1114: { day: 'wi wi-snow-wind', night: 'wi wi-night-alt-snow-wind' }, // Blowing snow
+  1117: { day: 'wi wi-snow-wind', night: 'wi wi-night-alt-snow-wind' }, // Blizzard
+  1135: { day: 'wi wi-fog', night: 'wi wi-fog' }, // Fog
+  1147: { day: 'wi wi-fog', night: 'wi wi-fog' }, // Freezing fog
+  1150: { day: 'wi wi-day-showers', night: 'wi wi-night-alt-showers' }, // Patchy light drizzle
+  1153: { day: 'wi wi-day-showers', night: 'wi wi-night-alt-showers' }, // Light drizzle
+  1168: { day: 'wi wi-day-rain-mix', night: 'wi wi-night-alt-rain-mix' }, // Freezing drizzle
+  1171: { day: 'wi wi-rain-mix', night: 'wi wi-rain-mix' }, // Heavy freezing drizzle
+  1180: { day: 'wi wi-day-showers', night: 'wi wi-night-alt-showers' }, // Patchy light rain
+  1183: { day: 'wi wi-showers', night: 'wi wi-showers' }, // Light rain
+  1186: { day: 'wi wi-showers', night: 'wi wi-showers' }, // Moderate rain at times
+  1189: { day: 'wi wi-showers', night: 'wi wi-showers' }, // Moderate rain
+  1192: { day: 'wi wi-rain', night: 'wi wi-rain' }, // Heavy rain at times
+  1195: { day: 'wi wi-rain', night: 'wi wi-rain' }, // Heavy rain
+  1198: { day: 'wi wi-day-rain-mix', night: 'wi wi-night-alt-rain-mix' }, // Light freezing rain
+  1201: { day: 'wi wi-rain-mix', night: 'wi wi-rain-mix' }, // Moderate or heavy freezing rain
+  1204: { day: 'wi wi-day-rain-mix', night: 'wi wi-night-rain-mix' }, // Light sleet
+  1207: { day: 'wi wi-rain-mix', night: 'wi wi-rain-mix' }, // Moderate or heavy sleet
+  1210: { day: 'wi wi-day-snow', night: 'wi wi-night-alt-snow' }, // Patchy light snow
+  1213: { day: 'wi wi-snow', night: 'wi wi-snow' }, // Light snow
+  1216: { day: 'wi wi-snow', night: 'wi wi-snow' }, // Patchy moderate snow
+  1219: { day: 'wi wi-snow', night: 'wi wi-snow' }, // Moderate snow
+  1222: { day: 'wi wi-snow', night: 'wi wi-snow' }, // Patchy heavy snow
+  1225: { day: 'wi wi-snow', night: 'wi wi-snow' }, // Heavy snow
+  1237: { day: 'wi wi-hail', night: 'wi wi-hail' }, // Ice pellets
+  1240: { day: 'wi wi-day-showers', night: 'wi wi-night-alt-showers' }, // Light rain shower
+  1243: { day: 'wi wi-showers', night: 'wi wi-showers' }, // Moderate or heavy rain shower
+  1246: { day: 'wi wi-rain', night: 'wi wi-rain' }, // Torrential rain shower
+  1249: { day: 'wi wi-day-rain-mix', night: 'wi wi-night-alt-rain-mix' }, // Light sleet showers
+  1252: { day: 'wi wi-rain-mix', night: 'wi wi-rain-mix' }, // Moderate or heavy sleet showers
+  1255: { day: 'wi wi-day-snow', night: 'wi wi-night-alt-snow' }, // Light snow showers
+  1258: { day: 'wi wi-snow', night: 'wi wi-snow' }, // Moderate or heavy snow showers
+  1282: { day: 'wi wi-day-hail', night: 'wi wi-night-alt-hail' }, // Light showers of ice pellets
+  1261: { day: 'wi wi-hail', night: 'wi wi-hail' }, // Moderate or heavy showers of ice pellets
+  1264: { day: 'wi wi-day-storm-showers', night: 'wi wi-night-alt-storm-showers' }, // Patchy light rain with thunder
+  1273: { day: 'wi wi-thunderstorm', night: 'wi wi-thunderstorm' }, // Moderate or heavy rain with thunder
+  1276: {
+    day: 'wi wi-day-snow-thunderstorm',
+    night: 'wi wi-night-alt-snow-thunderstorm',
+  }, // Patchy light snow with thunder
+  1279: {
+    day: 'wi wi-day-snow-thunderstorm',
+    night: 'wi wi-night-alt-snow-thunderstorm',
+  }, // Moderate or heavy snow with thunder
 };
 
 function getConditionIcon(code, isDay = 1) {
@@ -131,6 +144,7 @@ class TemperatureBar {
 
   /**
    * Draws a temperature bar with a gradient that spans the max range of temperatures.
+   * Uses a clip path.
    */
   draw() {
     this.main.style.backgroundImage = this.getGradient();
@@ -141,6 +155,7 @@ class TemperatureBar {
   /**
    * Draws a temperature bar with a gradient that represents the
    * min and max temperatures for the visible forecast.
+   * Uses a mask instead of a clip path.
    */
   drawType2() {
     this.main.style.backgroundImage = this.getGradientType2();
